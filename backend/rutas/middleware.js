2c1de543a9a65130143ef20de25355980912949e
Antonio <antonionavajasortega@uma.es>
2023-12-15 14:38:57 +0100

    oauth funciona

--- backend/rutas/middleware.js
+++ backend/rutas/middleware.js
@@ -1,16 +1,22 @@
+const { OAuth2Client } = require('google-auth-library');
+
+const client = new OAuth2Client("934310162741-e74bp9o07ua6kiit7kgeu0qfinljc8oh.apps.googleusercontent.com");
+
 
 const verifyTokenMiddleware = async (req,res,next) => {
     const token = req.header("Authorization");
-
+    console.log("Solicitud middleware")
     if (!token) {
+        console.log("no hay token")
         return res.status(401).json({succes: false, error: "Token no proporcionado"});
+        
     }
 
     try {
 
         const cleanedToken = token.replace("Bearer ", "");
 
-        const ticket = await cliente.verifyIdToken({
+        const ticket = await client.verifyIdToken({
                 idToken: cleanedToken,
                 audience: "934310162741-e74bp9o07ua6kiit7kgeu0qfinljc8oh.apps.googleusercontent.com"
         });
@@ -19,8 +25,9 @@ const verifyTokenMiddleware = async (req,res,next) => {
 
         req.user = payload;
 
-
+        next();
     } catch (error) {
+        console.log("token no válido", error.message);
         res.status(401).json({succes: false, error: "Token no válido"});
     }
 }
